8776f4cbb250f0c7af6b27f705093f8762c36222
Javed Hussain <javed.hussain@sina.pk>
2024-09-26 11:06:03 +0500

    navbar modified

--- car-buying/app/page.js
+++ car-buying/app/page.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <PreQualification />
       </>
   );
